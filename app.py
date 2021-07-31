0c2643d972a2523515f93eae48e295f52228e55b
Adolfo <amurillo2005@gmail.com>
2021-07-31 17:30:33 +0000

    website

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import get_lunch_rating
 
 
 # -- Initialization section --
@@ -11,6 +12,28 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/homepage')
 def index():
-    return "hello world"
+    return render_template("homepage.html")
+
+
+@app.route('/secret')
+def secret():
+    return render_template("kek.html")
+
+
+@app.route('/apple')
+def apple():
+    return render_template("apple.html")
+
+
+@app.route('/results', methods=["GET", "POST"])
+def results():
+    print(request.form["lunch"])
+    user_lunch = request.form["lunch"]
+    user_nickname = request.form["nickname"]
+    lunch_rating = get_lunch_rating(user_lunch)
+    if request.method == "POST":
+        return render_template("/results.html", user_lunch=user_lunch, user_nickname=user_nickname, lunch_rating=lunch_rating)
+    else:
+        return "404 error"
